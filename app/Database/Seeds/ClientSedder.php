0e6847fbfe2480bb0471341e1273c5ec22478bae
Julian Esteban Rios Holguin <desarrolladorjr4.spv@gmail.com>
2021-08-16 20:13:25 -0500

    first commit

---
+++ app/Database/Seeds/ClientSedder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use Faker\Factory;
+
+class ClientSedder extends Seeder
+{
+	public function run()
+	{
+		for ($i = 0; $i < 10; $i++) {
+			$this->db->table('client')->insert($this->generateClient());
+		}
+	}
+
+	private function generateClient(): array
+	{
+		$faker = Factory::create();
+
+		return [
+			'name' => $faker->name(),
+			'email' => $faker->email,
+			'retainer_fee' => random_int(10000, 100000000)
+		];
+	}
+
+}
